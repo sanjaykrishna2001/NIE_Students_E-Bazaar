43558d2ed8836c6f6d8f44c13b35dde7d25d4c35
sanjaykrishna2001 <msanjaykrishna27@gmail.com>
2022-06-24 21:49:16 +0530

    minor changes

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "https://nie-studends-ebazaar.herokuapp.com/login",
+      callbackURL: "https://nie-studends-ebazaar.herokuapp.com/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {

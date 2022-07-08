55a58f2262856356a40701107486a3681774be3e
sanjaykrishna2001 <msanjaykrishna27@gmail.com>
2022-07-08 20:27:51 +0530

    email restrict

--- app.js
+++ app.js
@@ -53,11 +53,20 @@ const userSchema = new mongoose.Schema({
   googleId: String,
 });
 
+const emailSchema=new mongoose.Schema({
+  email_Id: String,
+  admin: Boolean
+});
+
+
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 const Product = new mongoose.model("Product", productSchema);
+const EmailList =new mongoose.model("EmailList", emailSchema);
+
+
 
 passport.use(User.createStrategy());
 
@@ -76,7 +85,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "https://nie-studends-ebazaar.herokuapp.com/auth/google/home",
+      callbackURL: "http://localhost:3000/auth/google/home",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
@@ -103,8 +112,16 @@ app.get("/auth/google/home",
     // Successful authentication, redirect home.
     res.redirect('/home');
   });
-
-
+const a=[]
+app.get("/manage",function(req,res){
+  if (req.isAuthenticated()) {
+    EmailList.find({},function(err,email_list){
+      res.render("manage",{ user: req.user, EmailList:email_list});
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -112,7 +129,7 @@ app.get("/login", function (req, res) {
 app.get("/signup", function (req, res) {
   res.render("signup");
 });
-
+ 
 app.get("/", function (req, res) {
   if (req.isAuthenticated()) {
     res.redirect("/home");
@@ -123,11 +140,20 @@ app.get("/", function (req, res) {
 
 app.get("/home", function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("home", { user: req.user });
+    EmailList.findOne({email:req.user.email}, function (err, list) {
+      if (err)
+        console.log(err);
+      else {
+        res.render("home", { user: req.user, emailList:list });
+      }
+    });
+    // const mail=emailList.findOne({email:user.email})
+    // res.render("home", { user: req.user, });
   } else {
     res.redirect("/login");
   }
 });
+;
 
 app.get("/working", function (req, res) {
   if (req.isAuthenticated()) {
@@ -180,20 +206,20 @@ app.get("/donate", function (req, res) {
   res.redirect("/login");
 }
 });
-
+//items with given category are shown to the user
 app.get("/category/:type", function (req, res) {
-  if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) { //checks for for user authentication
     const type = req.params.type;
-    if (type === "All") {
+    if (type === "All") { // if the type is All then all item are shown
       Product.find({}, function (err, foundProducts) {
         if (err)
-          console.log(err);
-        else {
-          res.render("category", { user: req.user, product: foundProducts });
+          console.log(err); // if any error the error message is shown
+        else { //if no error then all items are shown
+          res.render("category", { user: req.user, product: foundProducts }); 
         }
       });
     }
-    else {
+    else { //if type is not ALL then the particular items with that type are shown
       Product.find({ type: type }, function (err, foundProducts) {
         if (err)
           console.log(err);
@@ -203,7 +229,7 @@ app.get("/category/:type", function (req, res) {
       });
     }
   } else {
-    res.redirect("/login");
+    res.redirect("/login");// if user not authenticated then its redirected to login page
   }
 });
 
@@ -255,13 +281,13 @@ app.get("/logout", function (req, res) {
 });
 
 app.post("/signup", function (req, res) {
+  //gets name, email and password from user and checks this user already present or not
   User.register({ username: req.body.username, name: req.body.fullname }, req.body.password, function (err, user) {
     if (err) {
       console.log(err);
       res.redirect("/login");
-    } else {
+    } else { // if the user not present then new user is created and authenticated
       passport.authenticate("local")(req, res, function () {
-        //console.log(req.user);
         const userEmail = req.user.username;
         User.findOneAndUpdate({ _id: req.user._id }, { $set: { email: userEmail } }, { upsert: true }, function (err, doc) {
           if (err)
@@ -269,28 +295,90 @@ app.post("/signup", function (req, res) {
           else
             console.log("updated");
         });
-        res.redirect("/home");
+        res.redirect("/home"); //after authentication its redirected to home page
       });
     }
   });
 });
 
+
 app.post("/login", function (req, res) {
+
+  // gets email and password from user
   const user = new User({
     username: req.body.username,
     password: req.body.password
   });
-
+  
   req.login(user, function (err) {
-    if (err)
+    if (err) // if any wrong email or password then the relavent error message is shown 
       console.log(err);
-    else {
+    else { //if the email and passwword are matched then that user gets authenticated
       passport.authenticate("local")(req, res, function () {
-        res.redirect("/home");
+        res.redirect("/home"); // after authentication its redirected to home page 
       });
     }
   });
 });
+
+app.post("/add_email",function(req,res){
+  EmailList.exists({email_Id:req.body.username_1},function(err,doc){
+    if(!err){
+      if(!doc){
+        let emailLists=new EmailList({
+          email_Id:req.body.username_1,
+          admin:false
+        }); 
+        emailLists.save();
+      }
+    }
+  });
+  res.redirect("/manage");
+});
+app.post("/delete_email",function(req,res){
+  EmailList.exists({email_Id:req.body.username_3},function(err,doc){
+    if(!err){
+      if(doc){
+        EmailList.findOneAndDelete({email_Id:req.body.username_3},function(err,doc){
+
+        });
+      }
+    }
+  });
+  res.redirect("/manage");
+})
+app.post("/add_admin",function(req,res){
+  EmailList.exists({email_Id:req.body.username_2},function(err,doc){
+    if(!err){
+      if(!doc){
+        let emailLists=new EmailList({
+          email_Id:req.body.username_2,
+          admin:true
+        }); 
+        emailLists.save();
+      }
+      else{
+        EmailList.findOneAndUpdate({email_Id:req.body.username_2},{admin:true},{new:true},function(err,doc){
+          //
+        });
+      }
+    }
+  });
+  res.redirect("/manage");
+});
+
+app.post("/delete_admin",function(req,res){
+  EmailList.exists({email_Id:req.body.username_4},function(err,doc){
+    if(!err){
+      if(doc){
+        EmailList.findOneAndUpdate({email_Id:req.body.username_4},{admin:false},{new:true},function(err,doc){
+
+        });
+      }
+    }
+  });
+  res.redirect("/manage");
+})
 
 // app.post("/add",function(req,res){
 //   //console.log(req.file);
@@ -310,27 +398,24 @@ app.post("/login", function (req, res) {
 
 app.post("/add", upload.single("image"), async (req, res) => {
   try {
-    //Upload image to cloudinary
-    //console.log(req.file);
     const result = await cloudinary.uploader.upload(req.file.path);
-
-    // Create new user
+    //when a seller add a item new document is created in product collection and those valuses are stored
     let product = new Product({
-      name: req.body.name,
-      price: req.body.price,
-      description: req.body.description,
-      type: req.body.radio,
-      seller: req.user._id,
-      sellerName: req.user.name,
-      image: result.secure_url,
+      name: req.body.name, //name of the item
+      price: req.body.price, // price of the item
+      description: req.body.description, // description of the item
+      type: req.body.radio, //type(category) of the item
+      seller: req.user._id, // here we also store seller details to know who is selling this item
+      sellerName: req.user.name, // seller name
+      image: result.secure_url, // image of the item
       cloudinary_id: result.public_id,
     });
-    // Save user
+    // document is saved
     await product.save();
-    //res.json(product);
+    //then the page is redirected to all products and the added product shown there
     res.redirect("/category/All");
   } catch (err) {
-    console.log(err);
+    console.log(err); //if any error the error message is shown
   }
 });
 
